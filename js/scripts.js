ac27e181d8d83ce1e91b62eb6a80266414ebdefb
Wesle Leal <wesle.profissional@gmail.com>
2020-04-22 07:51:14 -0300

    Script Scroll para Sections

--- js/scripts.js
+++ js/scripts.js
@@ -89,67 +89,101 @@ $(document).ready(function () {
 
     });
 
-   // iniciando loaders quando a usuário chegar no elemento
-  let dataAreaOffset = $('#data-area').offset();
-  // stop serve para a animação não carregar mais que uma vez
-  let stop = 0;
+    // iniciando loaders quando a usuário chegar no elemento
+    let dataAreaOffset = $('#data-area').offset();
+    // stop serve para a animação não carregar mais que uma vez
+    let stop = 0;
 
-  $(window).scroll(function (e) {
+    $(window).scroll(function (e) {
 
-    let scroll = $(window).scrollTop();
+        let scroll = $(window).scrollTop();
 
-    if(scroll > (dataAreaOffset.top - 500) && stop == 0) {
-      circleA.animate(1.0);
-      circleB.animate(1.0);
-      circleC.animate(1.0);
-      circleD.animate(1.0);
+        if (scroll > (dataAreaOffset.top - 500) && stop == 0) {
+            circleA.animate(1.0);
+            circleB.animate(1.0);
+            circleC.animate(1.0);
+            circleD.animate(1.0);
 
-      stop = 1;
+            stop = 1;
+        }
+
+    });
+
+    // Parallax
+
+    // setTimeout serve para carregar primeiro as imagens
+    setTimeout(function () {
+        $('#data-area').parallax({ imageSrc: 'img/cidadeparallax.png' });
+        $('#apply-area').parallax({ imageSrc: 'img/pattern.png' });
+    }, 200);
+
+    //filtro do portfolio
+
+    $('.filter-btn').on('click', function () {
+        let type = $(this).attr('id');
+        let boxes = $('.project-box');
+
+        $('.main-btn').removeClass('active');
+        $(this).addClass('active');
+
+        if (type == 'dsg-btn') {
+            eachBoxes('dsg', boxes);
+        } else if (type == 'dev-btn') {
+            eachBoxes('dev', boxes);
+        } else if (type == 'seo-btn') {
+            eachBoxes('seo', boxes);
+        } else {
+            eachBoxes('all', boxes);
+        }
+
+    });
+
+    function eachBoxes(type, boxes) {
+        if (type == 'all') {
+            $(boxes).fadeIn();
+        } else {
+            $(boxes).each(function () {
+                if (!$(this).hasClass(type)) {
+                    $(this).fadeOut('slow');
+                } else {
+                    $(this).fadeIn();
+                }
+            });
+        }
     }
 
-  });
+    // scroll para sections
+    let navBtn = $('.nav-item');
+    let bannerSection = $('#mainSlider');
+    let aboutSection = $('#about-area');
+    let servicesSection = $('#services-area');
+    let teamSection = $('#team-area');
+    let portfolioSection = $('#portfolio-area');
+    let contactSection = $('#contact-area');
 
-  // Parallax
+    let scrollTo = '';
 
-  // setTimeout serve para carregar primeiro as imagens
-  setTimeout(function() {
-    $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'});
-    $('#apply-area').parallax({imageSrc: 'img/pattern.png'});
-  }, 200);
+    $(navBtn).click(function () {
+        let btnId = $(this).attr('id');
 
-  //filtro do portfolio
+        console.log(btnId)
 
-  $('.filter-btn').on('click', function(){
-      let type = $(this).attr('id');
-      let boxes = $('.project-box');
+        if (btnId == 'about-menu') {
+            scrollTo = aboutSection;
+        } else if (btnId == 'services-menu') {
+            scrollTo = servicesSection;
+        } else if (btnId == 'team-menu') {
+            scrollTo = teamSection;
+        } else if (btnId == 'portfolio-menu') {
+            scrollTo = portfolioSection;
+        } else if (btnId == 'contact-menu') {
+            scrollTo = contactSection;
+        } else {
+            scrollTo = bannerSection
+        }
 
-      $('.main-btn').removeClass('active'); 
-      $(this).addClass('active');
-
-      if(type == 'dsg-btn'){
-        eachBoxes('dsg', boxes);
-      }else if (type == 'dev-btn') {
-          eachBoxes('dev', boxes);
-      } else if (type == 'seo-btn') {
-          eachBoxes('seo', boxes);
-      } else {
-          eachBoxes('all', boxes);
-      }
-
-  });
-
-  function eachBoxes(type, boxes) {
-      if(type == 'all') {
-          $(boxes).fadeIn();
-      } else {
-          $(boxes).each(function(){
-              if(!$(this).hasClass(type)) {
-                $(this).fadeOut('slow');  
-              } else {
-                  $(this).fadeIn();
-              }
-          });
-      }
-  }
-
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(scrollTo).offset().top - 70
+        }, 1500);
+    });
 });
